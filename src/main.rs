41a907c2adad29d3dc740c1d90e55e4bf899b7da
ddanielsantos <contatodanieljob@gmail.com>
2022-10-11 18:48:50 -0300

    feat: configure env file handling

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,32 @@
-use std::net::SocketAddr;
+use dotenvy::dotenv;
+use std::{
+    env,
+    error::Error,
+    net::{Ipv4Addr, SocketAddr},
+};
 mod routes;
 
 #[tokio::main]
-async fn main() {
-    let routes = routes::app_routes();
+async fn main() -> Result<(), Box<dyn Error>> {
+    dotenv().ok().expect(".env file not set");
+    let url = env::var("URL")
+        .expect("env variable URL not found")
+        .parse::<Ipv4Addr>()
+        .expect("kkk");
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let port = env::var("PORT")
+        .expect("env variable PORT not found")
+        .parse::<u16>()
+        .expect("not a valid port");
+
+    let addr = SocketAddr::from((url, port));
+
+    let routes = routes::app_routes();
 
     axum::Server::bind(&addr)
         .serve(routes.into_make_service())
         .await
         .unwrap();
+
+    Ok(())
 }
