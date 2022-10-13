d19085ec1b55d1e351663bc50d3fe3dbace79f49
ddanielsantos <contatodanieljob@gmail.com>
2022-10-13 00:04:51 -0300

    feat: fix env errors

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,17 @@
 use dotenvy::dotenv;
-use std::{
-    env,
-    error::Error,
-    net::{Ipv4Addr, SocketAddr},
-};
+use std::{env, error::Error, net::SocketAddr};
 mod routes;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    dotenv().ok()
+    dotenv().ok();
 
     let port = env::var("PORT")
         .expect("env variable PORT not found")
         .parse::<u16>()
         .expect("not a valid port");
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], port));
+    let addr = SocketAddr::from(([0, 0, 0, 0], port));
 
     println!("listening on {}", addr);
 
