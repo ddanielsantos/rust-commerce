156d7214084416b2f25160c02c218f28d74d10bc
ddanielsantos <contatodanieljob@gmail.com>
2022-10-14 16:50:24 -0300

    feat: move components to src/

---
+++ frontend/pages/product/[id].tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import { Header } from '../../src/components/Header/Header'
+
+const ProductPage: NextPage = () => {
+	return (
+		<div>
+			<Header />
+		</div>
+	)
+}
+
+export default ProductPage
