7867a19c714511b876c7b90597caabcc4254bf8a
ddanielsantos <contatodanieljob@gmail.com>
2022-10-14 18:18:01 -0300

    feat: improve header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,12 +1,26 @@
+import NextLink from 'next/link'
+
 export const Header = () => {
 	return (
 		<div
 			style={{
-				border: '1px solid black',
-				padding: '1.5rem'
+				outline: '1px solid black',
+				padding: '1.5rem',
+				display: 'flex',
+				justifyContent: 'space-between',
+				alignItems: 'center'
 			}}
 		>
-			<h2>buyrust</h2>
+			<NextLink href={'/product/1'}>buyrust</NextLink>
+			<div
+				style={{
+					display: 'flex',
+					gap: '10px'
+				}}
+			>
+				<button>login</button>
+				<span>0</span>
+			</div>
 		</div>
 	)
 }
