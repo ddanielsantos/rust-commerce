4e6e0c00ed2745abaabc9d02930e7413936fc948
ddanielsantos <contatodanieljob@gmail.com>
2022-10-17 15:16:51 -0300

    feat: create products table

---
+++ backend/src/db/migrations/2022-10-16-022252_create_products/up.sql
@@ -0,0 +1,6 @@
+-- Your SQL goes here
+
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR NOT NULL
+)
---
+++ backend/src/schema.rs
@@ -0,0 +1,8 @@
+// @generated automatically by Diesel CLI.
+
+diesel::table! {
+    products (id) {
+        id -> Int4,
+        name -> Varchar,
+    }
+}
