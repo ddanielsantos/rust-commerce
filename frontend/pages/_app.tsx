1beec3dafd41898ea789a25ed8945a30155ae461
ddanielsantos <contatodanieljob@gmail.com>
2022-10-24 08:28:21 -0300

    refactor: move css reset to stitches global styles

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,10 @@
-import './reset.css'
 import type { AppProps } from 'next/app'
+import { globalStyle } from '../src/ui/globalStyle'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
+	globalStyle()
+
 	return <Component {...pageProps} />
 }
 
-export default MyApp
+export default App
