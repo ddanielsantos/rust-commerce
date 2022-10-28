65c0fb887c6dfed410cafb68c88a871a0b7a1dc3
ddanielsantos <contatodanieljob@gmail.com>
2022-10-28 13:54:07 -0300

    feat: add grid

--- frontend/pages/cart/index.tsx
+++ frontend/pages/cart/index.tsx
@@ -1,38 +1,79 @@
 import type { NextPage } from 'next'
 import { Header } from '../../src/components/Header/Header'
 import { useCart } from '../../src/context/CartContext'
-import { Box, Flex, Text } from '../../src/ui'
+import { Box, Flex, Text, Grid, Heading } from '../../src/ui'
 
 const CartItemsList = () => {
-	const { items } = useCart()
+	const { numberOfItems, items } = useCart()
 
-	if (Object.keys(items).length === 0) {
+	if (numberOfItems === 0) {
 		return <Text>there are no items in the cart</Text>
 	}
 
 	return (
-		<>
+		<Flex>
 			{Object.keys(items).map(key => {
 				return (
 					<Box
 						key={key}
 						css={{
 							p: '$4',
-							backgroundColor: '$rust70',
+							width: '100%',
+							// border: '3px solid $rust100',
 							borderRadius: '$lg',
+							transition: 'ease 0.2s',
+							'&:hover': {
+								backgroundColor: '$rust50',
+								'&>span': {
+									color: 'White',
+								},
+							},
 						}}
 					>
 						<Text
+							as='span'
 							css={{
-								color: 'white',
+								fontWeight: 700,
+								color: '$rust50',
 							}}
 						>
-							id: {key} quantity: {items[key]}
+							cool item name here
+						</Text>
+						<Text
+							as='span'
+							css={{
+								color: 'White',
+							}}
+						>
+							{items[key]}
 						</Text>
 					</Box>
 				)
 			})}
-		</>
+		</Flex>
+	)
+}
+
+const CartTotalSum = () => {
+	const { numberOfItems } = useCart()
+
+	return (
+		<Box
+			css={{
+				border: '3px solid $rust100',
+				borderRadius: 10,
+				p: '$3',
+				'@bp1': {
+					p: '$5',
+				},
+			}}
+		>
+			{numberOfItems === 0 ? (
+				<Text>the cart is empty</Text>
+			) : (
+				<Text>Total sum:</Text>
+			)}
+		</Box>
 	)
 }
 
@@ -47,14 +88,43 @@ const CartPage: NextPage = () => {
 		>
 			<Header />
 			<Flex
-				align='center'
 				justify='center'
 				css={{
-					flex: 1,
 					width: '100%',
 				}}
 			>
-				<CartItemsList />
+				<Grid
+					alignContent='center'
+					css={{
+						maxWidth: '$5xl',
+						g: '$3',
+						gridTemplateColumns: '1fr',
+
+						p: '$3',
+						'@bp1': {
+							p: '$5',
+							gridTemplateColumns: '1fr $3xs',
+						},
+					}}
+				>
+					<Flex
+						direction={'column'}
+						css={{
+							border: '3px solid $rust100',
+							borderRadius: 10,
+
+							g: '$3',
+							p: '$3',
+							'@bp1': {
+								p: '$5',
+							},
+						}}
+					>
+						<Heading>Cart</Heading>
+						<CartItemsList />
+					</Flex>
+					<CartTotalSum />
+				</Grid>
 			</Flex>
 		</Flex>
 	)
