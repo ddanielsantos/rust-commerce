b12cc7f8d93387858857040d56b846c23b98aec6
Daniel Santos <contatodanieljob@gmail.com>
2022-11-01 12:16:04 -0300

    feat: add fields and placeholder values

--- frontend/src/components/CartTotalSum/CartTotalSum.tsx
+++ frontend/src/components/CartTotalSum/CartTotalSum.tsx
@@ -1,16 +1,33 @@
-import { Box, Text } from '../../ui'
+import { Grid, Text } from '../../ui'
+import { moneyFormatter } from '../../utils/MoneyFormatter'
 
 export const CartTotalSum = () => {
+	const total = moneyFormatter.format(50)
+
 	return (
-		<Box
+		<Grid
 			type='spaced'
 			borderVariant={'simple'}
 			roundedBorder
 			css={{
+				gap: '$3',
+				gridTemplateColumns: '1fr 1fr',
 				height: 'fit-content',
 			}}
 		>
-			<Text>Total sum:</Text>
-		</Box>
+			<Text>Total sum: </Text>
+			<Text>{total}</Text>
+			<Text>Discounts: </Text>
+			<Text>{total}</Text>
+			<hr
+				style={{
+					gridColumn: '1 / 3',
+					borderColor: '#ffffffaa',
+					borderWidth: 1,
+				}}
+			/>
+			<Text>Final price: </Text>
+			<Text>{total}</Text>
+		</Grid>
 	)
 }
