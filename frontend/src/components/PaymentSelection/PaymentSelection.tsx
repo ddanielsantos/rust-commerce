4c100867c615008129208b36992970d3d6cc4632
Daniel Santos <contatodanieljob@gmail.com>
2022-11-05 15:30:31 -0300

    fix: move modal away from label

--- frontend/src/components/PaymentSelection/PaymentSelection.tsx
+++ frontend/src/components/PaymentSelection/PaymentSelection.tsx
@@ -7,40 +7,38 @@ import { FormEvent } from 'react'
 export const PaymentSelection = () => {
 	const onSubmit = (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault()
-		console.log(payment_method.value)
-		payment_method.value === 'card' && console.log(card_id.value)
 	}
 
 	const payment_method = useRadioState()
 	const card_id = useSelectState({
-		defaultValue: '1',
-		sameWidth: true,
-		gutter: 4,
+		gutter: 10,
+		animated: true,
 	})
 	return (
 		<form onSubmit={onSubmit}>
 			<RadioGroup state={payment_method}>
 				<Label as='label'>
-					boleto
 					<Radio value='boleto' name='payment_method' />
+					boleto
 				</Label>
 				<Label as='label'>
+					<Radio value='card' name='payment_method' />
 					debit/credit card
-					<Flex direction='column' align='start' css={{ gap: '$3' }}>
-						<Radio value='card' name='payment_method' />
-						{payment_method.value === 'card' && (
-							<div>
-								<Select state={card_id} className='select' name='card' />
-								<SelectPopover state={card_id} className='popover'>
-									<AddCardModal />
-
-									<SelectItem className='select-item' value='card 1' />
-									<SelectItem className='select-item' value='card 2' />
-								</SelectPopover>
-							</div>
-						)}
-					</Flex>
 				</Label>
+
+				<Flex direction='column' align='start' css={{ gap: '$3' }}>
+					{payment_method.value === 'card' && (
+						<div>
+							<Select.Wrapper state={card_id} />
+							<Select.Popover state={card_id}>
+								<AddCardModal />
+
+								<Select.Item value='card 1' />
+								<Select.Item value='card 2' />
+							</Select.Popover>
+						</div>
+					)}
+				</Flex>
 			</RadioGroup>
 			<Button variant='primary' type='submit'>
 				send
