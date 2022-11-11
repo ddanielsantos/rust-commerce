26985da7e78832401d80c01fb5eededb9891705c
ddanielsantos <contatodanieljob@gmail.com>
2022-11-11 19:21:29 -0300

    style(checkout): selected item

--- apps/frontend/src/components/PaymentSelection/PaymentSelection.tsx
+++ apps/frontend/src/components/PaymentSelection/PaymentSelection.tsx
@@ -1,34 +1,81 @@
 import { Radio, RadioGroup, useRadioState } from 'ariakit/radio'
-import { useSelectState } from 'ariakit/select'
+import { SelectArrow, useSelectState } from 'ariakit/select'
 import { AddCardModal } from '../AddCardModal/AddCardModal'
-import { Flex, Label, Select } from '../../ui'
+import { Flex, Label, Select, Text } from '../../ui'
+
+type Card = {
+	id: string
+	card_number: string
+	expiryDate: string
+}
+
+const cards = [
+	{
+		id: '1',
+		card_number: 'xxxx xxxx xxxx 9999',
+		expiryDate: '08/29',
+	},
+	{
+		id: '2',
+		card_number: 'xxxx xxxx xxxx 9997',
+		expiryDate: '11/25',
+	},
+]
+
+const CardItem = ({ card }: { card: Card }) => {
+	return (
+		<Flex direction='column' align='start' css={{ gap: '$2' }}>
+			<Text css={{ fontWeight: 700 }}>{card.card_number}</Text>
+			<Text css={{ fontWeight: 500 }}>{card.expiryDate}</Text>
+		</Flex>
+	)
+}
 
 export const PaymentSelection = () => {
 	const payment_method = useRadioState()
 	const card_id = useSelectState({
 		gutter: 10,
 		animated: true,
+		focusLoop: true,
+		setValueOnMove: true,
 	})
+
+	const selectedCard = cards.find(c => c.id === card_id.value)
+
 	return (
 		<RadioGroup state={payment_method}>
-			<Label as='label'>
+			<Label as='label' css={{ flexDirection: 'row' }}>
 				<Radio value='boleto' name='payment_method' />
 				boleto
 			</Label>
-			<Label as='label'>
+			<Label
+				as='label'
+				css={{ flexDirection: 'row', '&>input': { appearence: 'none' } }}
+			>
 				<Radio value='card' name='payment_method' />
 				debit/credit card
 			</Label>
-
 			<Flex direction='column' align='start' css={{ gap: '$3' }}>
 				{payment_method.value === 'card' && (
 					<div>
-						<Select.Wrapper state={card_id} />
+						<Select.Wrapper state={card_id}>
+							{selectedCard ? (
+								CardItem({ card: selectedCard })
+							) : (
+								<Text>no registered cards</Text>
+							)}
+							<SelectArrow />
+						</Select.Wrapper>
 						<Select.Popover state={card_id}>
 							<AddCardModal />
 
-							<Select.Item value='card 1' />
-							<Select.Item value='card 2' />
+							{cards.map(c => {
+								return (
+									<Select.Item key={c.id} value={c.id}>
+										<CardItem card={c} />
+									</Select.Item>
+								)
+							})}
 						</Select.Popover>
 					</div>
 				)}
--- apps/frontend/src/components/ShippingAddressSelection/ShippingAddressSelection.tsx
+++ apps/frontend/src/components/ShippingAddressSelection/ShippingAddressSelection.tsx
@@ -1,21 +1,74 @@
-import { useSelectState } from 'ariakit/select'
+import { SelectArrow, useSelectState } from 'ariakit/select'
 
 import { AddAddressModal } from '../AddAddressModal/AddAddressModal'
-import { Flex, Select } from '../../ui'
+import { Flex, Select, Text } from '../../ui'
+
+type Address = {
+	id: string
+	city: string
+	state_or_province: string
+	country: string
+	location: string
+}
+
+const addresses = [
+	{
+		id: '1',
+		city: 'Paris',
+		state_or_province: 'NO',
+		country: 'France',
+		location: 'Rue Lepougain 424',
+	},
+	{
+		id: '2',
+		city: 'Saffron',
+		state_or_province: 'PK',
+		country: 'Kanto',
+		location: 'Misty Avenue 103',
+	},
+]
+
+const AddressItem = ({ addr }: { addr: Address }) => {
+	return (
+		<Flex direction='column' align='start' css={{ gap: '$2' }}>
+			<Text css={{ fontWeight: 700 }}>{addr.location}</Text>
+			<Text css={{ fontWeight: 500 }}>
+				{addr.country}, {addr.state_or_province}
+			</Text>
+		</Flex>
+	)
+}
 
 export const ShippingAddressSelection = () => {
 	const address_id = useSelectState({
 		gutter: 10,
 		animated: true,
+		focusLoop: true,
+		setValueOnMove: true,
 	})
+
+	const selectedAddr = addresses.find(a => a.id === address_id.value)
+
 	return (
 		<Flex direction='column' align='start' css={{ gap: '$3' }}>
-			<Select.Wrapper state={address_id} />
+			<Select.Wrapper state={address_id} css={{ gap: '$5' }}>
+				{selectedAddr ? (
+					AddressItem({ addr: selectedAddr })
+				) : (
+					<Text>no registered addresses</Text>
+				)}
+				<SelectArrow />
+			</Select.Wrapper>
 			<Select.Popover state={address_id}>
 				<AddAddressModal />
 
-				<Select.Item value='address 1' />
-				<Select.Item value='address 2' />
+				{addresses.map(a => {
+					return (
+						<Select.Item key={a.id} value={a.id}>
+							<AddressItem addr={a} />
+						</Select.Item>
+					)
+				})}
 			</Select.Popover>
 		</Flex>
 	)
