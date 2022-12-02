5edce7a728a9ded396098318fb96df4f0d1534df
Daniel Santos <80872981+ddanielsantos@users.noreply.github.com>
2022-12-02 14:27:53 -0300

    feat(server): add openapi and swagger (#32)
    * feat(server): add utoipa
    
    * feat(server): add OpenAPI documentation

--- apps/backend/src/main.rs
+++ apps/backend/src/main.rs
@@ -1,12 +1,18 @@
+mod routes;
+
 use std::{error::Error, net::SocketAddr};
 
-mod routes;
+use utoipa::OpenApi;
+use utoipa_swagger_ui::SwaggerUi;
+
+use routes::ApiDoc;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let addr = SocketAddr::from(([0, 0, 0, 1], 2000));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
 
-    let routes = routes::app_routes();
+    let routes = routes::app_routes()
+        .merge(SwaggerUi::new("/swagger-ui").url("/api-doc/openapi.json", ApiDoc::openapi()));
 
     println!("listening on {}", addr);
 
