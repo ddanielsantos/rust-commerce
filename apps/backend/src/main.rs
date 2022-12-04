328dbbe7c4da6f5284aa9535b6ce8e06c828cc0e
Daniel Santos <80872981+ddanielsantos@users.noreply.github.com>
2022-12-04 00:04:52 -0300

    refacto(server): change to SQLx (#49)

--- apps/backend/src/main.rs
+++ apps/backend/src/main.rs
@@ -2,22 +2,39 @@ mod routes;
 
 use std::{error::Error, net::SocketAddr};
 
+use axum::{Extension, Router};
 use utoipa::OpenApi;
 use utoipa_swagger_ui::SwaggerUi;
 
-use routes::ApiDoc;
+use sqlx::{postgres::PgPoolOptions, PgPool};
+
+use routes::{app_routes, ApiDoc};
+
+pub fn app(db: PgPool) -> Router {
+    Router::new()
+        .merge(app_routes())
+        .merge(SwaggerUi::new("/swagger-ui").url("/api-doc/openapi.json", ApiDoc::openapi()))
+        .layer(Extension(db))
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
 
-    let routes = routes::app_routes()
-        .merge(SwaggerUi::new("/swagger-ui").url("/api-doc/openapi.json", ApiDoc::openapi()));
+    let database_url = dotenvy::var("DATABASE_URL")
+        // The error from `var()` doesn't mention the environment variable.
+        .expect("DATABASE_URL must be set");
+
+    let db = PgPoolOptions::new()
+        .max_connections(20)
+        .connect(&database_url)
+        .await
+        .expect("could not connect to database");
 
     println!("listening on {}", addr);
 
     axum::Server::bind(&addr)
-        .serve(routes.into_make_service())
+        .serve(app(db).into_make_service())
         .await
         .unwrap();
 
